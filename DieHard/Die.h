5ae5c1ff04a65f9bbb3023978dd61579ad0898bb
Sherrie Jones <sj24kt@comcast.net>
2015-01-21 13:52:43 -0700

    finished DieHard project

---
+++ DieHard/Die.h
@@ -0,0 +1,13 @@
+//
+//  Die.h
+//  DieHard
+//
+//  Created by Sherrie Jones on 1/21/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Die : NSObject
+
+@end
---
+++ DieHard/Die.m
@@ -0,0 +1,13 @@
+//
+//  Die.m
+//  DieHard
+//
+//  Created by Sherrie Jones on 1/21/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "Die.h"
+
+@implementation Die
+
+@end
